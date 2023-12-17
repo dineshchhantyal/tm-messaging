7884b89e26a28ec6716f8c7820d1b6e10160029a
Dinesh Chhantyal <dineshchhantyal@Dineshs-Laptop.local>
2023-12-17 11:44:35 -0600

    Versioning untracked files

--- components/Header/Logo.tsx
+++ components/Header/Logo.tsx
@@ -1,5 +1,15 @@
+import { cn } from '@/lib/utils';
 import React from 'react';
 
-export const Logo = () => {
-  return <h1 className="font-bold text-2xl tracking-wide">Tramona</h1>;
+interface LogoProps extends React.HTMLAttributes<HTMLHeadingElement> {}
+
+export const Logo = ({ className, ...props }: LogoProps) => {
+  return (
+    <h1
+      className={cn('font-bold text-2xl tracking-wide', className ?? '')}
+      {...props}
+    >
+      Tramona
+    </h1>
+  );
 };
