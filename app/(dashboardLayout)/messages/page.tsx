5dfcc4111593c91cabedaa4f74461d24e192998d
Dinesh Chhantyal <dineshchhantyal@Dineshs-Laptop.local>
2023-12-17 11:48:51 -0600

    config: messages

--- app/(dashboardLayout)/messages/page.tsx
+++ app/(dashboardLayout)/messages/page.tsx
@@ -1,7 +1,14 @@
+import MessageTab from '@/components/MessageTab/MessageTab';
+import MessagesListing from '@/components/MessagesListing/MessagesListing';
 import React from 'react';
 
 const Messages = () => {
-  return <div>Messages</div>;
+  return (
+    <div>
+      <MessagesListing />
+      <MessageTab />
+    </div>
+  );
 };
 
 export default Messages;
