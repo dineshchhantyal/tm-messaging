6616cf165bfda76d4177a8ce088333dc41c10d29
Dinesh Chhantyal <myagdichhantyal@gmail.com>
2023-12-17 13:54:53 -0600

    feat: Add link to create messages page in layout

--- app/(dashboardLayout)/messages/layout.tsx
+++ app/(dashboardLayout)/messages/layout.tsx
@@ -3,8 +3,8 @@ import { Inter } from 'next/font/google';
 import Sidebar from '@/components/Sidebar/Sidebar';
 import { PlusSquare } from 'lucide-react';
 import MessagesListing from '@/components/MessagesListing/MessagesListing';
-import MessageTab from '@/components/MessageTab/MessageTab';
 import { cn } from '@/lib/utils';
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -25,10 +25,10 @@ export default function RootLayout({
           <Sidebar />
         </header>
         <div className="pl-3 mt-5">
-          <div className="flex gap-20 pl-5">
+          <Link href={'/messages'} className="flex gap-20 pl-5">
             <h1 className="font-bold text-4xl tracking-wide">Messages</h1>
             <PlusSquare className="w-5 h-5 self-end text-gray-400 cursor-pointer hover:text-gray-500" />
-          </div>
+          </Link>
           <section className="flex gap-5 mt-8">
             <MessagesListing />
             {children}
